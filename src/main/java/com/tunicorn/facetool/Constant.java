dfba87ff911096deef653df115c9b971fe37402a
vitas <vitas@DESKTOP-EO5PPEJ>
2018-05-20 08:50:24 +0800

    test

--- src/main/java/com/tunicorn/facetool/Constant.java
+++ src/main/java/com/tunicorn/facetool/Constant.java
@@ -3,30 +3,35 @@ package com.tunicorn.facetool;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * 
+ * @author vitas
+ *
+ */
 @Component
 public class Constant {
 
-    @Value("${face.app-id}")
-    public String appId;
+	@Value("${face.app-id}")
+	public String appId;
 
-    @Value("${face.authorization}")
-    public String authorization;
+	@Value("${face.authorization}")
+	public String authorization;
 
-    @Value("${face.detect-img-url}")
-    public String detectImgURL;
+	@Value("${face.detect-img-url}")
+	public String detectImgURL;
 
-    @Value("${face.detect-img-root-path}")
-    public String detectImgRootPath;
+	@Value("${face.detect-img-root-path}")
+	public String detectImgRootPath;
 
-    @Value("${face.compare-img-url}")
-    public String compareImgURL;
+	@Value("${face.compare-img-url}")
+	public String compareImgURL;
 
-    @Value("${face.compare-img-root-path}")
-    public String compareImgRootPath;
+	@Value("${face.compare-img-root-path}")
+	public String compareImgRootPath;
 
-    @Value("${face.second-interval}")
-    public float secondInterval;
+	@Value("${face.second-interval}")
+	public float secondInterval;
 
-    @Value("${face.finished-path-name}")
-    public String finishedPathName;
+	@Value("${face.finished-path-name}")
+	public String finishedPathName;
 }
